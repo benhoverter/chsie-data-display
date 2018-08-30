b199b0377453c0a5d0fc50fb3f290358d261cd38
Ben Hoverter <ben.hoverter@gmail.com>
2018-08-29 17:50:02 -0700

    Simplified constructor.  Added instructional comments.

--- chsie-data-display/includes/Main.php
+++ chsie-data-display/includes/Main.php
@@ -44,9 +44,9 @@ class CHSIE_Data_Display {
     *
     * @since    1.0.0
     * @access   protected
-    * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+    * @var      string    $plugin_title    The string used to uniquely identify this plugin.
     */
-    protected $plugin_name;
+    protected $plugin_title;
 
     /**
     * The current version of the plugin.
@@ -92,158 +92,62 @@ class CHSIE_Data_Display {
             $this->version = '1.0.0';
         }
 
-        $this->plugin_name = 'Plugin Name';
-        $this->load_dependencies_includes();
+        $this->plugin_title = 'Plugin Name';
+
+        // Add required files to set_dependencies():
+        $this->load_dependencies( $this->set_dependencies() );
+
+        // Instantiate the Loader object:
+        $this->loader = new Plugin_Abbr_Loader();
 
         // Localization.
         $this->set_locale();
 
-        // Add new module files to one of these:
-        $this->load_dependencies_admin();
-        $this->load_dependencies_public();
-        $this->load_dependencies_config();
-
-        // These shouldn't need modification:
-        $this->define_admin_asset_hooks();
-        $this->define_public_asset_hooks();
-
-        // The WeDevs Settings API interface:
-        $this->define_settings_hooks();
-
-        // Create a new hook definer for each module:
-        $this->define_admin_module_hooks();
-        $this->define_admin_module_ajax_hooks();
-
-        $this->define_public_module_hooks();
-        $this->define_public_module_ajax_hooks();
+        // Define the hooks and pass them to the Loader:
+        $this->define_hooks();
 
     }
 
 
     /**
-    * Load the dependencies from /includes.
-    *
-    * Include the following files that make up the plugin:
-    * - Loader.php. Orchestrates the hooks of the plugin.
-    * - I18n.php. Defines internationalization functionality.
-    *
-    * Create an instance of the loader which will be used to register the hooks
-    * with WordPress.
+    * Sets the file dependencies.
+    * Manually enter file paths here.
     *
     * @since    1.0.0
     * @access   private
+    * @return   array       $dependencies       The files to load, relative
+    *                                           to the plugin directory.
     */
-    private function load_dependencies_includes() {
+    private function set_dependencies() {
 
-        /**
-        * The class responsible for orchestrating the actions and filters of the
-        * core plugin.
+        /* Manually enter file paths here.
+        *  Paths must be relative to the plugin directory.
+        *  No leading slash: 'admin/Assets.php', not '/admin/Assets.php'.
         */
-        require_once plugin_dir_path( __DIR__ ) . 'includes/Loader.php';
+        $dependencies = array(
 
-        /**
-        * The class responsible for defining internationalization functionality
-        * of the plugin.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'includes/I18n.php';
+            // Includes:
+            'includes/I18n.php',
+            'includes/Loader.php',
 
-        // Instantiates the Loader object:
-        $this->loader = new CDD_Loader();
+            // Admin:
+            'admin/Assets.php',
+            'admin/Settings.php',
+            'admin/module/Module.php',
+            'admin/module-ajax/Module-Ajax.php',
 
-    }
+            // Public:
+            'public/Assets.php',
+            'public/module/Module.php',
+            'public/module-ajax/Module-Ajax.php',
 
+            // Config:
+            'config/Config.php',
+            'config/Queries.php'       // End of array.  No comma!
 
-    /**
-    * Load the required dependencies for the admin area.
-    *
-    * Include the following files that make up the plugin:
-    * - Assets.php. Versions and enqueues JS and CSS for the admin area.
-    * - Settings.php.  Defines the WeDevs Settings API integration.
-    * - Module.php.  Defines the stuff.
-    * - Module-Ajax.php.  Defines the AJAX stuff.
-    *
-    * @since    1.0.0
-    * @access   private
-    */
-    private function load_dependencies_admin() {
+        );
 
-        /**
-        * The class responsible for versioning and enqueueing the combined
-        * admin-area scripts and styles.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'admin/Assets.php';
-
-        /**
-        * The class responsible for defining all admin settings and menu options.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'admin/Settings.php';
-
-        /**
-        * The class responsible for the admin MODULE FUNCTIONS.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'admin/module/Module.php';
-
-        /**
-        * The class responsible for the admin MODULE-AJAX FUNCTIONS.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'admin/module-ajax/Module-Ajax.php';
-
-    }
-
-
-    /**
-    * Load the required dependencies for the public area.
-    *
-    * Include the following files that make up the plugin:
-    * - Assets.php. Versions and enqueues JS and CSS for the public area.
-    * - Module.php.  Defines the stuff.
-    * - Module-Ajax.php.  Defines the AJAX stuff.
-    *
-    * @since    1.0.0
-    * @access   private
-    */
-    private function load_dependencies_public() {
-
-        /**
-        * The class responsible for versioning and enqueueing the combined
-        * public-area scripts and styles.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'public/Assets.php';
-
-        /**
-        * The class responsible for the public MODULE FUNCTIONS.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'public/module/Module.php';
-
-        /**
-        * The class responsible for the public MODULE-AJAX FUNCTIONS.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'public/module-ajax/Module-Ajax.php';
-
-    }
-
-
-    /**
-    * Load the required dependencies for the database configuration and queries.
-    *
-    * Include the following files that make up the plugin:
-    * - Config.php. Defines all configuration options for the database connection.
-    * - Queries.php.  Defines the queries to run on the database.
-    *
-    * @since    1.0.0
-    * @access   private
-    */
-    private function load_dependencies_config() {
-
-        /**
-        * The class responsible for defining the database configuration options.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'config/Config.php';
-
-        /**
-        * The class responsible for defining the SQL queries run by the plugin.
-        */
-        require_once plugin_dir_path( __DIR__ ) . 'config/Queries.php';
+        return $dependencies;
 
     }
 
@@ -267,6 +171,35 @@ class CHSIE_Data_Display {
 
 
     /**
+    * Loads all file dependencies.
+    *
+    *
+    * @since    1.0.0
+    * @access   private
+    */
+    private function define_hooks() {
+
+        // These shouldn't need modification:
+        $this->define_admin_asset_hooks();
+        $this->define_public_asset_hooks();
+
+        // The WeDevs Settings API interface, if used:
+        $this->define_settings_hooks();
+
+        // Create a new hook definer method for each module:
+        $this->define_admin_module_hooks();
+        $this->define_admin_module_ajax_hooks();
+
+        $this->define_public_module_hooks();
+        $this->define_public_module_ajax_hooks();
+
+    }
+
+    // ************************ CLASS HOOK DEFINERS ************************ //
+
+    // ************* ASSET ENQUEUEING HOOKS ************* //
+    // ************* NOTE: NO NEED TO CHANGE ************ //
+    /**
     * Version and enqueue the JS and CSS for the admin side of the plugin.
     *
     * @since    1.0.0
@@ -274,13 +207,12 @@ class CHSIE_Data_Display {
     */
     private function define_admin_asset_hooks() {
 
-        $admin_assets = new CDD_Admin_Assets( $this->get_plugin_name(), $this->get_version() );
+        $admin_assets = new CDD_Admin_Assets( $this->get_plugin_title(), $this->get_version() );
 
         $this->loader->add_action( 'admin_enqueue_scripts', $admin_assets, 'enqueue_styles' );
         $this->loader->add_action( 'admin_enqueue_scripts', $admin_assets, 'enqueue_scripts' );
 
     }
-
 
     /**
     * Version and enqueue the JS and CSS for the public side of the plugin.
@@ -290,14 +222,15 @@ class CHSIE_Data_Display {
     */
     private function define_public_asset_hooks() {
 
-        $public_assets = new CDD_Public_Assets( $this->get_plugin_name(), $this->get_version() );
+        $public_assets = new CDD_Public_Assets( $this->get_plugin_title(), $this->get_version() );
 
         $this->loader->add_action( 'wp_enqueue_scripts', $public_assets, 'enqueue_styles' );
         $this->loader->add_action( 'wp_enqueue_scripts', $public_assets, 'enqueue_scripts' );
 
     }
 
-
+    // *************** SETTINGS API HOOKS *************** //
+    // ************* NOTE: NO NEED TO CHANGE ************ //
     /**
     * Register all of the hooks related to the WeDevs Settings API functionality
     * of the plugin.
@@ -307,7 +240,7 @@ class CHSIE_Data_Display {
     */
     private function define_settings_hooks() {
 
-        $plugin_settings = new CDD_Settings( $this->get_plugin_name(), $this->get_version() );
+        $plugin_settings = new CDD_Settings( $this->get_plugin_title(), $this->get_version() );
 
         // Standard functions that call dev-defined sections and menus in the Settings class:
         $this->loader->add_action( 'admin_menu', $plugin_settings, 'admin_menu' );
@@ -316,7 +249,7 @@ class CHSIE_Data_Display {
     }
 
 
-    // ************************* ADMIN MODULE HOOKS ************************* //
+    // ************* ADMIN MODULE HOOKS ************* //
     /**
     * Register all of the hooks related to the admin module functionality.
     *
@@ -325,7 +258,7 @@ class CHSIE_Data_Display {
     */
     private function define_admin_module_hooks() {
 
-        $module = new CDD_Admin_Module( $this->get_plugin_name(), $this->get_version() );
+        $module = new CDD_Admin_Module( $this->get_plugin_title(), $this->get_version() );
 
         // Standard hooks go here:
         //$this->loader->add_action( 'add_meta_boxes{_post_type}', $module->element, 'render_metabox' );
@@ -333,7 +266,6 @@ class CHSIE_Data_Display {
         $this->loader->add_action( 'admin_init', $module, 'render_view' );
 
     }
-
 
     /**
     * Register all of the hooks related to the admin module-ajax functionality.
@@ -343,7 +275,7 @@ class CHSIE_Data_Display {
     */
     private function define_admin_module_ajax_hooks() {
 
-        $module_ajax = new CDD_Admin_Module_Ajax( $this->get_plugin_name(), $this->get_version() );
+        $module_ajax = new CDD_Admin_Module_Ajax( $this->get_plugin_title(), $this->get_version() );
 
         // Standard hooks go here:
         //$this->loader->add_action( 'add_meta_boxes{_post_type}', $module_ajax, 'render_metabox' );
@@ -356,7 +288,7 @@ class CHSIE_Data_Display {
     }
 
 
-    // ************************* PUBLIC MODULE HOOKS ************************* //
+    // ************* PUBLIC MODULE HOOKS ************* //
     /**
     * Register all of the hooks related to the admin module functionality.
     *
@@ -365,7 +297,7 @@ class CHSIE_Data_Display {
     */
     private function define_public_module_hooks() {
 
-        $module = new CDD_Public_Module( $this->get_plugin_name(), $this->get_version() );
+        $module = new CDD_Public_Module( $this->get_plugin_title(), $this->get_version() );
 
         // Standard hooks go here:
         //$this->loader->add_action( 'add_meta_boxes{_post_type}', $module->element, 'render_metabox' );
@@ -375,7 +307,6 @@ class CHSIE_Data_Display {
 
     }
 
-
     /**
     * Register all of the hooks related to the admin module-ajax functionality.
     *
@@ -384,7 +315,7 @@ class CHSIE_Data_Display {
     */
     private function define_public_module_ajax_hooks() {
 
-        $module_ajax = new CDD_Public_Module_Ajax( $this->get_plugin_name(), $this->get_version() );
+        $module_ajax = new CDD_Public_Module_Ajax( $this->get_plugin_title(), $this->get_version() );
 
         // Standard hooks go here:
         //$this->loader->add_action( 'add_meta_boxes{_post_type}', $module_ajax, 'render_metabox' );
@@ -397,7 +328,8 @@ class CHSIE_Data_Display {
     }
 
 
-    // ************************* UTILITY FUNCTIONS ************************* //
+    // ************************* UTILITY METHODS ************************* //
+    // ********************* NOTE: NO NEED TO CHANGE ********************* //
     /**
     * Run the loader to execute all of the hooks with WordPress.
     *
@@ -415,8 +347,8 @@ class CHSIE_Data_Display {
     * @since     1.0.0
     * @return    string    The name of the plugin.
     */
-    public function get_plugin_name() {
-        return $this->plugin_name;
+    public function get_plugin_title() {
+        return $this->plugin_title;
     }
 
 
@@ -477,6 +409,22 @@ class CHSIE_Data_Display {
     }
 
 
+    /**
+    * Loads all file dependencies.
+    *
+    *
+    * @since    1.0.0
+    * @access   private
+    */
+    private function load_dependencies( $files ) {
+
+        foreach( $files as $file ) {
+
+            require_once plugin_dir_path( __DIR__ ) . $file;
+
+        }
+
+    }
 
 
 }
