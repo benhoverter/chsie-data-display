f22b90fc25a6102ca57a435b443dc4dfe6c23b47
Ben Hoverter <ben.hoverter@gmail.com>
2018-08-31 16:05:03 -0700

    Matching Boilerplate changes.
    Added documentation to custom settings page API.

--- chsie-data-display/admin/settings/Settings.php
+++ chsie-data-display/admin/settings/Settings.php
@@ -175,24 +175,54 @@ class CDD_Settings {
     * Defines the settings page sections in an associative array.
     *
     * Modify values and number of elements for your needs.
-    * Altering names in get_settings_sections() requires matching alteration
-    * in get_settings_fields().
+    * Altering 'id' values in get_settings_sections() requires
+    * matching alterations in get_settings_fields().
+    *
+    * NOTE: This array can take FOUR possible keys!
+    *
+    *           'id' and 'title' are required.
+    *
+    *           However, you can add either a CUSTOM DESCRIPTION or
+    *           a CUSTOM CALLBACK FUNCTION. The description takes precedence,
+    *           and WeDevs_Settings_API::admin_init() cannot display both.
+    *
+    *           You add a description with a 'desc' key whose value is a string
+    *           (HTML is OK, and escaped).
+    *
+    *           You add a description with a 'callback' key whose value is an array
+    *           holding the object instance containing the function and a string
+    *           with the name of the callback function.
+    *           (This is array callable syntax:
+    *               http://php.net/manual/en/language.types.callable.php ).
+    *           ************* This callback should echo its output *************
     *
     * @since    Custom addition for WeDevs Settings API.
     *
     */
     private function get_settings_sections() {
         $sections = array(
+
+            array(
+                'id'    => $this->plugin_slug . '_custom_settings',
+                'title' => __( 'Custom Settings', 'textdomain' ),
+                //'desc' => '<p class="description">This is a custom description.</p>',
+                'callback' => array(
+                    $this,
+                    'do_data_table'
+                )
+            ),
+
             array(
                 'id'    => $this->plugin_slug . '_basic_settings',
                 'title' => __( 'Basic Settings', 'textdomain' )
             ),
-            array(
-                'id'    => $this->plugin_slug . '_advanced_settings',
-                'title' => __( 'Advanced Settings', 'textdomain' )
-            )
+
         );
         return $sections;
+    }
+
+    public function do_data_table() {
+        include( plugin_dir_path( __FILE__ ) . 'views/view-name.php' ) ;
     }
 
 
